b836edbc16df866db1b0795cd54e76681b67ffec
brucegatete <bgatete@college.harvard.edu>
2017-06-23 16:39:20 -0700

    Version with the ratings

--- app/src/main/java/com/codepath/flicks/MovieAdapter.java
+++ app/src/main/java/com/codepath/flicks/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.flicks;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.codepath.flicks.models.Config;
 import com.codepath.flicks.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -100,20 +103,44 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 //create the view holder as a static inner class
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-        // track view objects
-        ImageView ivPosterImage;
-        ImageView ivBackdropImage;
-        TextView tvTitle;
-        TextView tvOverview;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            //lookup view objects by id
-            ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
-            ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
-            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
-            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+
+        // track view objects
+        @Override
+        public void onClick(View view) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+
+            }
+        }
+
+
+            ImageView ivPosterImage;
+            ImageView ivBackdropImage;
+            TextView tvTitle;
+            TextView tvOverview;
+
+        public ViewHolder(View itemView) {
+                super(itemView);
+                //lookup view objects by id
+                ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+                ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
+                tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+                tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+                // add this as the itemView's OnClickListener
+                itemView.setOnClickListener(this);
+
+            }
         }
     }
-}
